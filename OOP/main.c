4a4ef9f8b6c96e6482e0c36d49bbe42cd795cf18
RaZvan2703 <razvanradulescu38@gmail.com>
2025-03-09 22:07:59 +0200

    Linked List

--- OOP/main.c
+++ OOP/main.c
@@ -1,5 +1,29 @@
 #include <stdio.h>
 
+#define OPTIONS 4
+
+/*linked list of strings*/
+typedef struct Node {
+	char* data;
+	struct Node *next;
+}Node;
+
+int prime(int n) {
+	if (n % 2 == 0 || n < 2 )
+}
+
+void menu() {
+	options = {
+		"Prime numbers less than N",
+		"",
+		"",
+		"",
+	};
+	for (int i = 0; i < OPTIONS; i++) {
+		printf("%d. %s\n", i + 1, options[i]);
+	}
+}
+
 int main() {
 	printf("Hello, World!\n");
 	return 0;
